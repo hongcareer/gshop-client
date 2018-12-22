39c0c264ee619a3b297f9278672d2309bcc05f05
hongcareer <hongcareer@126.com>
2018-12-22 19:36:01 +0800

    异步请求api完成

---
+++ src/Api/index.js
@@ -0,0 +1,10 @@
+import ajax from './ajax'
+// http://localhost:5000/position/:geohash
+// /api
+const BASE = '/api'
+export const reqPosition=(longitude,latitude)=>ajax(BASE+`/position/${latitude},${longitude}`)
+//获取用户列表
+export const reqCategorys = () =>ajax(BASE+'/index_category')
+
+//根据经纬度获取商铺列表
+export const reqShops = (longitude,latitude)=>ajax(BASE+`/position/shops`,{longitude,latitude})
